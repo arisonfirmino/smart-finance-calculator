6d09b4537bdef335f30d226e9f6ab0b16213f417
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-03-09 17:09:15 -0300

    feat: add initial components for adding a new transaction on button click

--- app/components/add-transaction.tsx
+++ app/components/add-transaction.tsx
@@ -1,12 +1,47 @@
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@/app/components/ui/dialog";
 import { Button } from "@/app/components/ui/button";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/components/ui/tabs";
 
 import { PlusIcon } from "lucide-react";
 
 const AddTransaction = () => {
   return (
-    <Button size="icon">
-      <PlusIcon />
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="icon">
+          <PlusIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <VisuallyHidden>
+          <DialogTitle></DialogTitle>
+          <DialogDescription></DialogDescription>
+        </VisuallyHidden>
+
+        <Tabs defaultValue="account">
+          <TabsList>
+            <TabsTrigger value="transaction">Transação</TabsTrigger>
+            <TabsTrigger value="bank">Banco</TabsTrigger>
+          </TabsList>
+          <TabsContent value="transaction">
+            Adicionar nova transação
+          </TabsContent>
+          <TabsContent value="bank">Adicionar novo banco</TabsContent>
+        </Tabs>
+      </DialogContent>
+    </Dialog>
   );
 };
 
